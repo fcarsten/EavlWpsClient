1d6f52dbc669dff931bb79fe66a394230632f51d
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2014-11-05 16:57:10 +1100

    LogDensity Implemented

--- src/main/java/org/auscope/eavl/wpsclient/ConditionalProbabilityWpsClient.java
+++ src/main/java/org/auscope/eavl/wpsclient/ConditionalProbabilityWpsClient.java
@@ -30,6 +30,7 @@ public class ConditionalProbabilityWpsClient extends EavlWpsClient {
     }
 
     public final static String IMPUTATION_NA_SERVICE_ID = "org.n52.wps.server.r.impna";
+    public final static String LOG_DENSITY_SERVICE_ID="org.n52.wps.server.r.logDensity";
 
     public String imputationNA(int codaFirstC, int codaLastC, String csvData)
             throws WPSClientException, IOException {
@@ -62,6 +63,47 @@ public class ConditionalProbabilityWpsClient extends EavlWpsClient {
                 logger.warn("Could not delete temporary result file: " + f);
             }
             return result;
+        } catch (XPathExpressionException e) {
+            throw new WPSClientException("Error parsing result: "
+                    + e.getMessage(), e);
+        }
+
+    }
+
+    /**
+     * @param logDensityData String of Comma separated values
+     * @return
+     * @throws WPSClientException
+     * @throws IOException
+     */
+    public String[] logDensity(String logDensityData) throws WPSClientException, IOException {
+        ProcessDescriptionType pd = requestDescribeProcess(LOG_DENSITY_SERVICE_ID);
+        if (pd == null) {
+            throw new WPSClientException("imputationNA Algorithm not found on server!");
+        }
+
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("data",logDensityData);
+
+        ExecuteResponseAnalyser analyser= executeProcess(serviceUrl, LOG_DENSITY_SERVICE_ID, pd, parameters);
+
+        GenericFileDataBinding data = (GenericFileDataBinding) analyser
+                .getComplexData("output", GenericFileDataBinding.class);
+        GenericFileData fileData = (GenericFileData) data.getPayload();
+        File f = fileData.getBaseFile(false);
+
+        XPathFactory factory = javax.xml.xpath.XPathFactory.newInstance();
+        XPath xpath = factory.newXPath();
+        try {
+            XPathExpression expression = xpath.compile("/xml-fragment");
+            String result = expression
+                    .evaluate(new org.xml.sax.InputSource(
+                            new FileInputStream(f)));
+            if (!f.delete()) {
+                logger.warn("Could not delete temporary result file: " + f);
+            }
+
+            return result.split("[\n\r]+");
         } catch (XPathExpressionException e) {
             throw new WPSClientException("Error parsing result: "
                     + e.getMessage(), e);
