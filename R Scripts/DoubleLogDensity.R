eac7d9926fd468410e05aa1faa88428374b02cb7
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2014-11-13 13:50:05 +1100

    Fixed incorrect WPS4R syntax for input v

--- R Scripts/DoubleLogDensity.R
+++ R Scripts/DoubleLogDensity.R
@@ -1,28 +1,10 @@
 # wps.des: doubleLogDensity, title = Double Log Density;
 # wps.in: dataStr, string, DATA seperated by comma in one line;
-# wps.in: v, threshold value for the predictor
+# wps.in: v, double, threshold value for the predictor;
 # wps.off;
 dataStr=paste("0.007,65.34",
               "0.0095,59.795",
-              "0.02,47.02",
-              "0.0495,52.34",
-              "0.041,78.59",
-              "0.00675,93.95",
-              "0.00825,86.365",
-              "0.00825,71.615",
-              "0.0025,57.69",
-              "0.00775,55.095",
-              "0.1855,69.515",
-              "1.0855,66.875",
-              "0.959,56.965",
-              "0.2455,52.115",
-              "0.212,49.405",
-
-              "0.045,39.194",
-              "0.0045,34.836",
-              "0.0025,41.4",
-              "0.00425,38.425",
-              "0.181,51.38",
+ 
               "0.356,68.49", sep=",")
 v=0.437
 # wps.on;
