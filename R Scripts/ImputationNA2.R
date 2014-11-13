80acc0399349874f8c7ebc5ead51d445f6794039
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2014-11-14 10:23:25 +1100

    Handles missing values correctly now (which kind of is the point of this function....)

--- R Scripts/ImputationNA2.R
+++ R Scripts/ImputationNA2.R
@@ -3,15 +3,16 @@
 # wps.in: dataStr, string, DATA seperated by comma in one line;
 # wps.off;
 nCols=5 #CHANGE IN EAVL TO SPECIFIC COLUMN SELECTION VIA GUI?
-dataStr="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"
+dataStr="640.0,328.0,NA,169.0,196.0,1800.0,484.0,515.0,2291.0,912.0,2085.0,445.0,725.0,8373.0,1732.0,616.0,331.0,126.0,117.0,149.0,875.0,368.0,191.0,290.0,275.0,770.0,364.0,196.0,242.0,236.0,990.0,415.0,284.0,588.0,420.0,414.0,305.0,94.0,68.0,112.0,1394.0,440.0,393.0,1161.0,636.0,1285.0,374.0,363.0,785.0,487.0,1102.0,469.0,243.0,496.0,388.0,1717.0,452.0,452.0,1977.0,832.0,1549.0,454.0,424.0,1345.0,676.0,838.0,386.0,155.0,208.0,222.0,845.0,386.0,211.0,317.0,280.0,1130.0,394.0,271.0,490.0,386.0,1765.0,466.0,524.0,2133.0,822.0,1195.0,443.0,329.0,974.0,523.0,2180.0,521.0,553.0,2781.0,1010.0,1017.0,410.0,225.0,419.0,345.0"
 # wps.on;
 #THIS FILE CONTAINS THE METHODS FOR RUNNING THE R SCRIPT
 library(robCompositions) #robust composition, missing data
 
 #1. IMPORT
 # compdata=read.csv(datafile)
-dataVector=read.table(header=F, sep=",", text=dataStr)
-data=matrix(dataVector, nrow=length(dataVector)/nCols, ncol = nCols, byrow=T)
+dataVector=read.table(header=F, sep=",", text=dataStr, na.strings=c("","NaN","NA"))
+data=data.frame(matrix(t(dataVector), ncol=nCols, byrow=TRUE))
+#data=data.frame(matrix(dataVector, nrow=length(dataVector)/nCols, ncol = nCols, byrow=T))
 #2. PREPROCESSING
 
 #2.1. this version does not do DL substitution
